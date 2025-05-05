ccd007558df0d0038a8eef83ba6904d8fd1a2b6d
Janis Lauskis <janis.lauskis@protonmail.com>
2025-05-05 23:13:01 +0300

    Small styling

--- resources/views/livewire/galleries/images/gallery-image-show.blade.php
+++ resources/views/livewire/galleries/images/gallery-image-show.blade.php
@@ -17,15 +17,15 @@
                 <h1 class="text-2xl font-bold dark:text-white">
                     {{ $image['title'] ?? $image['original_filename'] }}
                 </h1>
-                <div class="flex space-x-2">
-                    <a href="{{ route('galleries.image.edit', [$gallery['id'], $image['id']]) }}" class="px-4 py-2 bg-blue-600 hover:bg-blue-700 text-white rounded-md flex items-center">
-                        <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <div class="flex items-center space-x-3">
+                    <a href="{{ route('galleries.image.edit', [$gallery['id'], $image['id']]) }}" class="px-3 py-2 bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 text-gray-700 dark:text-gray-200 rounded-md flex items-center transition-colors">
+                        <svg class="w-4 h-4 mr-1.5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path>
                         </svg>
                         Edit
                     </a>
-                    <button class="px-4 py-2 bg-red-600 hover:bg-red-700 text-white rounded-md flex items-center">
-                        <svg class="w-4 h-4 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <button class="px-3 py-2 bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 text-gray-700 dark:text-gray-200 hover:text-red-600 dark:hover:text-red-400 rounded-md flex items-center transition-colors cursor-pointer">
+                        <svg class="w-4 h-4 mr-1.5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
                         </svg>
                         Delete
