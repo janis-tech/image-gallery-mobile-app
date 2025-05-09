c55e158ebbfaec562b17802ec2be3f5c9ed385c9
Janis Lauskis <janis.lauskis@protonmail.com>
2025-05-09 20:19:11 +0300

    Fixed automated tests

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -6,8 +6,9 @@ test('guests are redirected to the login page', function () {
     $this->get('/dashboard')->assertRedirect('/login');
 });
 
-test('authenticated users can visit the dashboard', function () {
+test('authenticated users are redirected to galleries list page', function () {
     $this->actingAs($user = User::factory()->create());
 
-    $this->get('/dashboard')->assertStatus(200);
+    $this->get('/dashboard')->assertStatus(302)
+        ->assertRedirect('/galleries');
 });
